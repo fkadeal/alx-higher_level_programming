72df81674ab80ae2a6f63de37197d224f31a50b1
fkadeal <infkadeal@gmail.com>
2021-05-21 04:39:09 -0700

    py testing

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" add_integer() furncitoin add given integer type and 
+retrun the value , check for error [type error] """
+
+
+def add_integer(a, b=98):
+    """ integer addition function  """
+    
+    """ args if a is not integer and if it is float first must be converted to int
+        
+        args if b is not integer and if it is float first must be converted to int  """
+
+    if not isinstance(a,int) and not isinstance(a,float):
+        raise TypeError ("a is a must be an integer")
+    elif not isinstance(b,int) and not isinstance(b,float):
+        raise TypeError ("b is a must be an integer")
+    if isinstance(b,float):
+        bb = int(b)
+    elif isinstance(b,int):
+        bb = b
+    if isinstance(a,float):
+        aa = int(a)
+    elif isinstance(a,int):
+        aa = a
+    return(aa+bb)
