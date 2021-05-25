7e72c4e5564796e95d0f3d5852a4b807099797f0
fkadeal <infkadeal@gmail.com>
2021-05-25 13:37:27 -0700

    4 line

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
-def magic_string(i=[0]):
+#!/usr/bin/python3
+def magic_string(i=[0]): 
     i[0]+=1
-    for u in range(int(i[0])):
-        print("Holberton ,", end =" ")
+    return int(i[0])*( "Holberton, ")
