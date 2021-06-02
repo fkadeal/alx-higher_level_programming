54659d1a66cfb3b36e9478c0f825e7ce4016c3d4
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-06-02 11:06:11 -0700

    update task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,23 +1,23 @@
+#!/bin/user/python3
+
 def pascal_triangle(n):
-    """
+     """
     Create a function def pascal_triangle(n):
     that returns a list of lists of integers representing
     the Pascals triangle of n
     """
-
-    new_list = []
+    
     if n <= 0:
-        return new_list
-    new_list = [[1]]
-    if n == 1:
-            return new_list
-    else:
-        n -= 1
-        new_list = [[1], [1, 1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(0, len(new_list[i]) - 1):
-            row.extend([new_list[i][j] + new_list[i][j+1]])
-        row += [1]
-        new_list.append(row)
-    return new_list
+        return []
+    temp = []
+    l = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if i == 0 or j == 0 or i == j:
+                row.append(1)
+            else:
+                row.append(l[j] + l[j - 1])
+        l = row
+        temp.append(row)
+    return temp
