e9038ff19d13e54664911c8556bfb2a9dbebca0f
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-08-03 04:55:10 -0700

    Create 2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function main(args){
+  var argv = args.length
+if(argv == 1){
+console.log('Argument found');
+}elseif(argv == 0){
+  console.log('No argument');
+}elseif(argv > 1){
+  console.log('Argument found');
+}
+}
