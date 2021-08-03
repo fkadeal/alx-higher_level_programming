b705a0a9ef4c17471516f27027078fb3da551473
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-08-03 05:37:22 -0700

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const _argv = process.argv.length;
 if (_argv === 3) {
-  console.log ('Argument found');
+  console.log('Argument found');
 } else if (_argv === 2) {
   console.log('No argument');
 } else if (_argv > 3) {
-  console.log ('Arguments found');
+  console.log('Arguments found');
 }
