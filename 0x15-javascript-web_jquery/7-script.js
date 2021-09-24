aefcbfbadc29912a6e8a118ae781d9a12fe59a17
fkadeal <infkadeal@gmail.com>
2021-09-24 04:54:47 -0700

    fixing typo

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,3 +1,3 @@
-ON( "https://swapi-api.hbtn.io/api/people/5/?format=json", function( json ) {
+$.getJSON('https://swapi-api.hbtn.io/api/people/5/?format=json', function (json) {
 	    $('#character').html(json.name);
 });
